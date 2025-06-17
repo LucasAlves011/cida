fc45f2275e27e097317d8436471e94ce60835a31
Lucas Alves <lucas.nascimento.al16@gmail.com>
2025-06-17 01:08:14 -0300

    Refatorando profile loader e modificando a arquitetura do projeto

--- profile_loader.ps1
+++ profile_loader.ps1
@@ -5,4 +5,10 @@
 
 $CAMINHO_BASE = $PSScriptRoot #Caminho da pasta onde este script roda
 $CAMINHO_PASTA_DOCUMENTOS = [System.IO.Path]::Combine($env:USERPROFILE, 'Documents')
-$CAMINHO_JSON_FUNCTION_NAMES = [System.IO.Path]::Combine($CAMINHO_BASE, 'resources\function_names.json')
+
+$CAMINHO_FAVICON = "$CAMINHO_BASE./resources/favicon.ico"
+
+$PASTA_PAI_PROJETO = [System.IO.Directory]::GetParent($PSScriptRoot).FullName
+$CAMINHO_JSON_FUNCTION_NAMES = [System.IO.Path]::Combine($PASTA_PAI_PROJETO, '\data\function_names.json')
+$CAMINHO_FUNCOES_PS1 = [System.IO.Path]::Combine($PASTA_PAI_PROJETO , '\data\funcoes.ps1')
+$CAMINHO_PASTA_SCRIPTS = [System.IO.Path]::Combine($PASTA_PAI_PROJETO , '\data\scripts')
